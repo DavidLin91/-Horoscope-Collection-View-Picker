0a1e61e90e2f6aeac4666baef08cd2fff047e91a
David <lin.david985@gmail.com>
2020-01-14 10:37:25 -0500

    updated UI

--- HoroscopeUserDefaultLab/CollectionViewCell.swift
+++ HoroscopeUserDefaultLab/CollectionViewCell.swift
@@ -10,5 +10,12 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var horoscopeImage: UIImageView!
+    @IBOutlet weak var birthdayLabel: UILabel!
     
+    
+    
+    func configureCell(horoscope: Horoscopes) {
+        birthdayLabel.text = horoscope.date
+        horoscopeImage.image = UIImage(named: "\(horoscope.sunsign.lowercased())")
+    }
 }
--- HoroscopeUserDefaultLab/HoroscopeVC.swift
+++ HoroscopeUserDefaultLab/HoroscopeVC.swift
@@ -14,26 +14,64 @@ class HoroscopeVC: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var userLabel: UITextField!
     @IBOutlet weak var horoscopeDescriptionLabel: UIImageView!
-    @IBOutlet weak var horoscopeDescriptionTextView: UITextView!
+    @IBOutlet weak var descriptionSunSignLabel: UILabel!
+    @IBOutlet weak var sunSignDescription: UILabel!
+    @IBOutlet weak var birthdayTextBox: UITextField!
     
+    
+    
+    
+    private var horoscopes = [Horoscopes]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
+    }
     
     override func viewDidLayoutSubviews() {
         enterNameButton.layer.cornerRadius = 5.0
         horoscopeDescriptionLabel.layer.cornerRadius = 10.0
-        
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
+        loadData()
     }
-
+    
+    
+    func loadData() {
+        HoroscopeAPIClient.getHoroscope { (result) in
+            switch result {
+            case .failure(let appError):
+                print(appError)
+            case .success(let data):
+                self.horoscopes = data
+            }
+        }
+    }
     
     
     @IBAction func enterButtonPressed(_ sender: UIButton) {
-        nameLabel.text = "Welcome \(userLabel.text ?? "")!"
+        nameLabel.text = "Name: \(userLabel.text ?? "") - Birthday: \(birthdayTextBox.text ?? "")"
     }
     
-
 }
 
+extension HoroscopeVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        horoscopes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "horoscopeImage", for: indexPath) as? CollectionViewCell else {
+            fatalError()
+        }
+        let horoscope = horoscopes[indexPath.row]
+        cell.configureCell(horoscope: horoscope)
+        descriptionSunSignLabel.text = horoscope.sunsign
+        sunSignDescription.text = horoscope.horoscope
+        return cell
+    }
+}
